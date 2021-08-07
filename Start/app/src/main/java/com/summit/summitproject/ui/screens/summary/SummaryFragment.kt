31486aae7650f14b8fdbf293a2fd0d3d6e768314
Yashar Atajan <yaxarat@users.noreply.github.com>
2021-08-07 19:37:04 -0400

    Have summary fragment pre-built
    Since this is mostly copy-paste from the login fragment, no need to re-do it here.

---
+++ Start/app/src/main/java/com/summit/summitproject/ui/screens/summary/SummaryFragment.kt
@@ -0,0 +1,21 @@
+package com.summit.summitproject.ui.screens.summary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class SummaryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            // UI goes here!
+        }
+    }
+}
