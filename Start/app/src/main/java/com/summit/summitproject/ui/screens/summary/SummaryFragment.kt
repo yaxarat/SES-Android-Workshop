6a2df2641b92f853143746e7d05151c4ac5dacb6
Yashar Atajan <yaxarat@users.noreply.github.com>
2021-08-08 11:00:55 -0400

    Update instructions

--- Start/app/src/main/java/com/summit/summitproject/ui/screens/summary/SummaryFragment.kt
+++ Start/app/src/main/java/com/summit/summitproject/ui/screens/summary/SummaryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -27,7 +28,7 @@ class SummaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View = ComposeView(requireContext()).apply {
         setContent {
-            // UI goes here!
+            Text("SummaryFragment")
         }
     }
 }
---
+++ instructions/step-by-step/4 - Fragment Transaction/README.md
@@ -0,0 +1,2 @@
+#  Fragment Transaction
+blash
